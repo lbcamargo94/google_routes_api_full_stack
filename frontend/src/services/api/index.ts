a2bfa158dc87f4b009752a9fc9b8177a9327c375
Lucas Camargo <lb.camargo94@gmail.com>
2024-11-27 23:41:11 -0300

    fix(backend): fix docker config

--- frontend/src/services/api/index.ts
+++ frontend/src/services/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const PORT = process.env.APPLICATION_PORT;
+const PORT = process.env.APPLICATION_PORT || "3001";
 
 const BACKEND_BASE_URL = `http://localhost:${PORT}`;
 
